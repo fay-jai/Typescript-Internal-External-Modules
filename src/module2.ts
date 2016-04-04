e23e3f2e41dc1d755da7dca1c05e7d3c5703b715
Willson Mock <willson.mock@gmail.com>
2016-04-03 17:21:11 -0700

    Example of Typescript internal module

---
+++ src/module2.ts
@@ -0,0 +1,24 @@
+namespace InternalModule {
+	export function add(...args: number[]) {
+		return args.reduce((acc, cur) => acc + cur, 0);
+	}
+}
+
+export = InternalModule;
+
+/*
+NOTE: this is the compiled JavaScript, assuming CommonJS module format
+"use strict";
+var InternalModule;
+(function (InternalModule) {
+    function add() {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i - 0] = arguments[_i];
+        }
+        return args.reduce(function (acc, cur) { return acc + cur; }, 0);
+    }
+    InternalModule.add = add;
+})(InternalModule || (InternalModule = {}));
+module.exports = InternalModule;
+*/
